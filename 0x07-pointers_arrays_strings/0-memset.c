5935bdae1879b46276367998fe544ae47d566597
themokaya <osinde.mg@gmail.com>
2022-11-01 08:02:47 -0700

    Even more pointers arrays strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,10 +10,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
-	unsigned cha *memory = s, value = b;
+	unsigned char *memory = s, value = b;
 
 	for (index = 0; index < n; index++)
-		memory[index] = = value;
+		memory[index] = value;
 
 	return (memory);
 }
